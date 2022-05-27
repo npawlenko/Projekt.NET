910e004e9cc942c4f9cb1cfe36e13abfe1462274
konrad121970 <konrad121970@windowslive.com>
2022-05-27 16:02:25 +0200

    Dodaj pliki projektów.

---
+++ Models/MyPageModel.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Projekt.NET.Models
+{
+    public class MyPageModel : PageModel
+    {
+
+        public MyPageModel()
+        {
+
+        }
+    }
+}
